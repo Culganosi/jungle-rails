049d1c3943d6cdc182642627afdcdeb8b21f458d
Culganosi <culgan_@hotmail.com>
2022-06-08 15:54:21 -0400

    add feature: product detail spec

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,19 @@
+describe('displays Jungle Home page', () => {
+  it('displays jungle home page', () => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 11);
+  });
+
+  it("should have the product be clickable and render product detail page ", () => {
+    cy.get(".products article > a > img").first().click();
+
+    cy.get(".product-detail")
+  });
+});
